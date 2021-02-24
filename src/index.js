a1f3c4d1e8e7e2f0cec49fd270130e02947a8bd6
AlfredoMora06 <agmmendez123@gmail.com>
2021-02-24 14:35:35 -0500

    icon

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+<link
+    rel="stylesheet"
+    href="https://maxcdn.bootstrapcdn.com/bootstrap/4.6.0/css/bootstrap.min.css"
+    integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
+    crossorigin="anonymous"
+/>
+
 function Square(props) {
     const className = 'square' + (props.highlight ? ' highlight' : '');
     return (
